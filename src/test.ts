4719bb29b3575a8dd45ee32c55bd3f4aa463f7b9
Kleber Freire <ch4r4d4@gmail.com>
2022-03-16 13:57:40 -0300

    teste jest

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-const numero = 2;
-
-if (numero === 2 || numero === 3) {
-  console.log("data");
-} else {
-  console.log("verdade");
-}
---
+++ src/testando.spec.ts
@@ -0,0 +1,10 @@
+{
+  describe("first", () => {
+    it("should a test", () => {
+      const receiver = 2;
+      const expected = 2;
+
+      expect(receiver).toBe(expected);
+    });
+  });
+}
